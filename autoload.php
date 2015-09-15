3b678fae41d56fa44e2819c74181292a4fcfb734
Martin Jonas <martin.jonas@growjob.com>
2015-09-15 15:14:46 +0200

    Added autoload.php

---
+++ autoload.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once __DIR__ . '/src/MetisFW/Database/Utils/Exceptions/FileNotFoundException.php';
+require_once __DIR__ . '/src/MetisFW/Database/Utils/Exceptions/NotSupportedException.php';
+require_once __DIR__ . '/src/MetisFW/Database/Utils/SqlImport.php';
+require_once __DIR__ . '/src/MetisFW/Database/Utils/DatabaseManagement.php';
