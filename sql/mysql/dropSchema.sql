5278a6ef1d7d4b8e5bbb9d795f6a9ff83347a80e
Martin Jonas <martin.jonas@growjob.com>
2015-12-10 15:22:56 +0100

    Fixed table name escaping in MySql dropSchema.sql - second version

--- sql/mysql/dropSchema.sql
+++ sql/mysql/dropSchema.sql
@@ -6,7 +6,7 @@ SET FOREIGN_KEY_CHECKS=0;
 CREATE TABLE dropSchema_no_tables_fallback (id int)
 
 SET @tables = NULL;
-SELECT GROUP_CONCAT('`', table_schema, '.', table_name, '`') INTO @tables
+SELECT GROUP_CONCAT('`', table_schema, '`.`', table_name, '`') INTO @tables
   FROM information_schema.tables 
   WHERE table_schema = @dbname;
 
